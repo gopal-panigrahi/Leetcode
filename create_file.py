8d7856c8dea8f84afe571341ad4f01f9d68bb987
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-05-20 14:45:22 +0530

    create file script

---
+++ create_file.py
@@ -0,0 +1,16 @@
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(prog="Create File")
+parser.add_argument("difficulty")
+parser.add_argument("name")
+parser.add_argument("extension")
+
+args = parser.parse_args()
+
+Path(args.difficulty.title(), args.name.title()).mkdir(parents=True, exist_ok=True)
+Path(
+    args.difficulty.title(),
+    args.name.title(),
+    f"{args.name.title().replace(' ', '')}.{args.extension.lower()}",
+).touch()
