ac910433982e7b3424fd070f141795627ae51b8a
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-05-29 21:12:04 +0530

    The Number of Beautiful Subsets

---
+++ Medium/The Number Of Beautiful Subsets/TheNumberOfBeautifulSubsets.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    unordered_map<int, int> count;
+    int solve(vector<int> &nums, int index, int k)
+    {
+        if (index >= nums.size())
+        {
+            return 1;
+        }
+
+        int ex = solve(nums, index + 1, k);
+
+        int in = 0;
+        if (count[nums[index] - k] == 0 && count[nums[index] + k] == 0)
+        {
+            count[nums[index]]++;
+            in = solve(nums, index + 1, k);
+            count[nums[index]]--;
+        }
+
+        return in + ex;
+    }
+
+    int beautifulSubsets(vector<int> &nums, int k)
+    {
+        return solve(nums, 0, k) - 1;
+    }
+};
