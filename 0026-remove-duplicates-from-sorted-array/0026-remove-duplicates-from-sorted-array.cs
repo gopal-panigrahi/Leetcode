a28fc5e4bfb90545b6e06a1171ba2a22cce42ab0
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-11-17 18:50:28 +0530

    Time: 0 ms (100%), Space: 50 MB (46.31%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,14 +1,12 @@
 
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        Dictionary<int, bool> seen = new Dictionary<int, bool>();
         int currIndex = 0;
-        for(int i = 0; i < nums.Length; i++) {
-            if(!seen.ContainsKey(nums[i])) {
-                nums[currIndex++] = nums[i]; 
-                seen[nums[i]] = true;
+        for(int i = 1; i < nums.Length; i++) {
+            if(nums[currIndex] != nums[i]) {
+                nums[++currIndex] = nums[i];
             }
         }
-        return currIndex;
+        return currIndex+1;
     }
 }
