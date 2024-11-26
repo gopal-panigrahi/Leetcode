fe31f59ea7e5adcd5abbce09fe869fde20605f79
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-11-26 07:59:24 +0530

    Time: 691 ms (63.4%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
-select e.name from Employee e, (
+select e.name from Employee e where e.id in (
     select e.managerId from Employee e group by e.managerId having count(managerId) >= 5
-) m where e.id = m.managerId;
+);
+
