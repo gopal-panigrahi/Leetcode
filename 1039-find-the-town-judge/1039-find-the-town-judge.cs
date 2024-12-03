7e1f99e7e8f8c7ed29137274af81d4e42fa454c0
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-12-03 09:52:16 +0530

    Time: 2 ms (67.05%), Space: 61 MB (100%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cs
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cs
@@ -1,14 +1,14 @@
 public class Solution {
     public int FindJudge(int n, int[][] trust) {
-        int[,] degree_table  = new int[2,n];
+        int[] degree_table  = new int[n];
 
         for(int i = 0; i < trust.Length; i++) {
-            degree_table[0,trust[i][0]-1] += 1;
-            degree_table[1,trust[i][1]-1] += 1;
+            degree_table[trust[i][0]-1] -= 1;
+            degree_table[trust[i][1]-1] += 1;
         }
 
         for(int i = 0; i < n; i++) {
-            if(degree_table[0,i] == 0 && degree_table[1,i] == (n-1)) {
+            if(degree_table[i] == (n-1)) {
                 return i+1;
             }
         }
