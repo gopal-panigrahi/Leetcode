648f6ad5a159bf6c9a681a58c0cc131b53cc937f
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-12-13 21:02:45 +0530

    Time: 6 ms (99.02%), Space: 61.6 MB (78.15%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cs
+++ 0729-my-calendar-i/0729-my-calendar-i.cs
@@ -1,24 +1,61 @@
 public class MyCalendar
 {
-    List<(int, int)> _calender;
+    public class TreeNode
+    {
+        public int startTime, endTime;
+        public TreeNode left, right;
+
+        public TreeNode(int _startTime, int _endTime)
+        {
+            startTime = _startTime;
+            endTime = _endTime;
+            left = null;
+            right = null;
+        }
+    }
+
+    TreeNode _root = null;
 
     public MyCalendar()
     {
-        _calender = new List<(int, int)>() { (0, 0) };
+        _root = new TreeNode(0, 0);
+    }
+
+    public bool Insert(TreeNode root, TreeNode node)
+    {
+        if (root.endTime <= node.startTime)
+        {
+            if (root.right == null)
+            {
+                root.right = node;
+                return true;
+            }
+            else
+            {
+                return Insert(root.right, node);
+            }
+        }
+        else if (node.endTime <= root.startTime)
+        {
+            if (root.left == null)
+            {
+                root.left = node;
+                return true;
+            }
+            else
+            {
+                return Insert(root.left, node);
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public bool Book(int startTime, int endTime)
     {
-        bool flag = true;
-        foreach (var (slotStart, slotEnd) in _calender)
-        {
-            flag &= (endTime <= slotStart || startTime >= slotEnd);
-        }
-        if (flag)
-        {
-            _calender.Add((startTime, endTime));
-        }
-        return flag;
+        return Insert(_root, new TreeNode(startTime, endTime));
     }
 }
 
