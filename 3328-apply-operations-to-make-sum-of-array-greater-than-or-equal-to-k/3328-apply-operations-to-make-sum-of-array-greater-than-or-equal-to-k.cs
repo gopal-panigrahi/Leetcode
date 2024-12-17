175cebef672e19eca36739a819af4d91b18c52c1
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-12-17 10:53:30 +0530

    Time: 0 ms (100%), Space: 27.8 MB (20%) - LeetHub

--- 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cs
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cs
@@ -3,14 +3,6 @@ public class Solution
     public int MinOperations(int k)
     {
         double limit = Math.Floor(Math.Sqrt(k));
-        int minOps = Int32.MaxValue;
-
-        for (double i = limit; i > 0; i--)
-        {
-            double ops = Math.Ceiling(k / i) + i - 2.0;
-            minOps = Math.Min(minOps, (int)ops);
-        }
-
-        return minOps;
+        return (int)(Math.Ceiling(k / limit) + limit - 2.0);
     }
 }
