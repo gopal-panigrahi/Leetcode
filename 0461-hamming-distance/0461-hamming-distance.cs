5fb55cbc20d46ff9c0583b6f577d75621740ea5f
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-12-28 09:08:35 +0530

    Time: 0 ms (100%), Space: 27.4 MB (35.46%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public int HammingDistance(int x, int y)
+    {
+        int count = 0;
+        while (x > 0 || y > 0)
+        {
+            if (((x & 1) ^ (y & 1)) == 1)
+            {
+                count++;
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        return count;
+    }
+}
