b9d1f0490e136a13d92b830eb71cb47067fbaea6
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-12-31 12:17:21 +0530

    Time: 7 ms (85.71%), Space: 45.6 MB (72.62%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cs
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cs
@@ -1,35 +1,45 @@
 public class Solution
 {
-    public int GetMinutes(string time)
+    public int ConvertToMinutes(string time)
     {
         string hh, mm;
 
         hh = time.Split(':')[0];
         mm = time.Split(':')[1];
 
-
-
         return (Convert.ToInt32(hh) * 60) + Convert.ToInt32(mm);
     }
 
     public int FindMinDifference(IList<string> timePoints)
     {
-        int minMinutes = Int32.MaxValue;
+        int minDiff = Int32.MaxValue;
 
-        timePoints = timePoints.OrderBy((x) => GetMinutes(x)).Select((x) => $"{GetMinutes(x)}").ToList();
-        timePoints.Add($"{Convert.ToInt32(timePoints[0]) + 1440}");
+        bool[] seen = new bool[1440];
 
-        for (int i = 1; i < timePoints.Count; i++)
+        foreach (var time in timePoints)
         {
-            int smaller = Convert.ToInt32(timePoints[i - 1]);
-            int larger = Convert.ToInt32(timePoints[i]);
-
-            int diff1 = Math.Abs(smaller - larger);
-            int diff2 = Math.Abs(smaller + 1440 - larger);
-
-            minMinutes = Math.Min(minMinutes, Math.Min(diff1, diff2));
+            if (seen[ConvertToMinutes(time)])
+            {
+                return 0;
+            }
+            seen[ConvertToMinutes(time)] = true;
         }
 
-        return minMinutes;
+        for (int curr = 0; curr < 1440; curr++)
+        {
+            if (seen[curr] == true)
+            {
+
+                int next = curr + 1;
+                while (!seen[next % 1440])
+                {
+                    next++;
+                }
+
+                minDiff = Math.Min(minDiff, Math.Min(Math.Abs(curr - next), Math.Abs(curr + 1440 - next)));
+            }
+        }
+
+        return minDiff;
     }
 }
