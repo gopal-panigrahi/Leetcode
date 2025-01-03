cdf316d89bd3b6b28a0098b88b4f4a0825e207aa
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2025-01-03 19:36:10 +0530

    Time: 0 ms (100%), Space: 27.9 MB (25.91%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cs
+++ 0670-maximum-swap/0670-maximum-swap.cs
@@ -1,78 +1,32 @@
 public class Solution
 {
-    public List<int> SplitIntoDigits(int num)
-    {
-        var l = new List<int>();
-        while (num > 0)
-        {
-            l.Add(num % 10);
-            num /= 10;
-        }
-        l.Reverse();
-        return l;
-    }
-
-    public Stack<(int, int)> GenerateMonotonicStack(List<int> digits)
-    {
-        var stk = new Stack<(int, int)>();
-
-        for (int i = digits.Count - 1; i >= 0; i--)
-        {
-            if (stk.Count == 0)
-            {
-                stk.Push((digits[i], i));
-            }
-            else
-            {
-                var (e, _) = stk.Peek();
-                if (digits[i] >= e)
-                {
-                    stk.Push((digits[i], i));
-                }
-            }
-        }
-        return stk;
-    }
-
-    public List<int> SwapToMakeItMaximum(Stack<(int, int)> stk, List<int> digits)
-    {
-        for (int i = 0; i < digits.Count; i++)
-        {
-            if (stk.Count > 0)
-            {
-                var (e, p) = stk.Peek();
-                if (digits[i] == e)
-                {
-                    stk.Pop();
-                }
-                else if (digits[i] < e)
-                {
-                    stk.Pop();
-                    while (stk.Count > 0)
-                    {
-                        var (te, tp) = stk.Peek();
-                        if (te != e)
-                        {
-                            break;
-                        }
-                        p = tp;
-                        stk.Pop();
-                    }
-
-                    (digits[i], digits[p]) = (digits[p], digits[i]);
-                    break;
-                }
-            }
-        }
-        return digits;
-    }
-
     public int MaximumSwap(int num)
     {
-        var digits = SplitIntoDigits(num);
-        var pq = GenerateMonotonicStack(digits);
-        digits = SwapToMakeItMaximum(pq, digits);
+        var digits = new StringBuilder(num.ToString());
+        int left = -1, right = -1, max = digits.Length - 1;
 
-        return Convert.ToInt32(string.Join(string.Empty, digits));
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] > digits[max])
+            {
+                max = i;
+                continue;
+            }
+
+            if (digits[i] < digits[max])
+            {
+                left = i;
+                right = max;
+            }
+        }
+
+        if (left == -1)
+        {
+            return num;
+        }
+
+        (digits[left], digits[right]) = (digits[right], digits[left]);
+
+        return Convert.ToInt32(digits.ToString());
     }
 }
