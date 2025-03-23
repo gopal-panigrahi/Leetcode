cbb4271f80a1943889a3aec55cc7b10db1ecebbe
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2025-03-23 16:23:22 +0530

    Time: 0 ms (100%), Space: 41 MB (38.61%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cs
+++ 0812-rotate-string/0812-rotate-string.cs
@@ -3,20 +3,7 @@ public class Solution {
         if(s.Length != goal.Length) {
             return false;
         }
-        for(int i = 0; i < s.Length; i++) {
-            bool flag = true;
-            int k = i;
-            for(int j = 0; j < goal.Length; j++) {
-                if(s[k%s.Length] != goal[j]) {
-                    flag = false;
-                    break;
-                }
-                k++;
-            }
-            if(flag) {
-                return true;
-            }
-        }        
-        return false;
+        string result = s + s;
+        return result.Contains(goal);
     }
 }
