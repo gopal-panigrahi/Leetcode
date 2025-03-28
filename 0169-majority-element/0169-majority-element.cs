8947c692c1cac50581b5b5d77c2fa100e03fdcb0
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2025-03-28 10:44:20 +0530

    Time: 1 ms (70.39%), Space: 49.5 MB (82.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,19 +1,23 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-        var freq = new Dictionary<int, int>();
-
-        int maxFreq = 0, element = -1;
-        for(int i = 0; i < nums.Length; i++) {
-            if(!freq.ContainsKey(nums[i])) {
-                freq[nums[i]] = 0;
-            }
-            freq[nums[i]]++;
-            if(maxFreq < freq[nums[i]]) {
-                maxFreq = freq[nums[i]];
+        int element = nums[0], freq = 1;
+        for(int i = 1; i < nums.Length; i++) {
+            if(freq == 0) {
                 element = nums[i];
+            }
+            if(nums[i] == element) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        freq = 0;
+        for(int i = 0; i < nums.Length; i++) {
+            if(nums[i] == element) {
+                freq++;
             }
         }
 
-        return element; 
+        return element;
     }
 }
